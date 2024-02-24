e77988c4f78846db8ac1298f9e8aa6cf5285ea9a
Joe Bulfer <jbulfer13@gmail.com>
2024-02-24 12:26:19 -0600

    added side view click events to swap description

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function getText(id){
 }
 
 
+
 /*
  so far I have added IDs to rects in inkscape and adding description. If actual visible text on SVG of is "Lisp Machine" id of rect should be "lisp-machine" words seperated with a dash
 
@@ -12,3 +13,22 @@ let javascriptDesc = getText("javascript")
 console.log(javascript) 
   javascript is a scripting language for the web .......
 */
+
+let sideView = document.getElementById("sideView")
+
+function swapSideView(id){
+	var svgText = document.getElementById(id).textContent;
+	sideView.innerHTML = svgText; 
+}
+
+//swapSideView("javascript")//this works
+
+//document.getElementById("docker").addEventListener("click", () => swapSideView("docker"));
+
+function swapEvent(el){
+    document.getElementById(el).addEventListener("click", () => swapSideView(el));
+}
+
+let events = ["alan-turing","van-nuemann-model","eniac","multics","unix","c","lisp-machine","gnu","debian","red-hat","java","javascript","rest-and-web-standardization","go","docker","kubernetes"]
+
+events.forEach((event) => swapEvent(event)); 
