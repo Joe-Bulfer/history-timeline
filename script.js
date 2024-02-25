995f55271ca7b1607865b97f2eeca83e3064b7b2
Joe Bulfer <jbulfer13@gmail.com>
2024-02-25 08:25:56 -0600

    event listeners and stylesheet

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+
+/* just mostly for testing/exirimenting
 function getText(id){
 	var svgTextElement = document.getElementById(id);
 	return svgTextElement.textContent;
 }
+*/
 
 
 
 /*
- so far I have added IDs to rects in inkscape and adding description. If actual visible text on SVG of is "Lisp Machine" id of rect should be "lisp-machine" words seperated with a dash
+ so far I have added IDs to rects in inkscape and added description. If actual visible text on SVG is "Lisp Machine" id of rect should be "lisp-machine" words seperated with a dash
 
  EXAMPLE
-let javascriptDesc = getText("javascript")
-console.log(javascript) 
+let javascriptDesc = getText("javascript") //arg is id of rect
+console.log(javascriptDesc) 
   javascript is a scripting language for the web .......
 */
 
+// just a div for now
 let sideView = document.getElementById("sideView")
 
 function swapSideView(id){
@@ -25,10 +29,12 @@ function swapSideView(id){
 
 //document.getElementById("docker").addEventListener("click", () => swapSideView("docker"));
 
-function swapEvent(el){
+function addSwapEvent(el){
     document.getElementById(el).addEventListener("click", () => swapSideView(el));
 }
 
+// make sure these match up with id's in inkscape or manually check rects
 let events = ["alan-turing","van-nuemann-model","eniac","multics","unix","c","lisp-machine","gnu","debian","red-hat","java","javascript","rest-and-web-standardization","go","docker","kubernetes"]
 
-events.forEach((event) => swapEvent(event)); 
+events.forEach((event) => addSwapEvent(event)); 
+/* currently the text element gets in the way so you have to click on the outer parts of the rect*/
